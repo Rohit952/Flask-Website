eebc42f4d2162798c5c9772772ca6f656e101b6b
unknown <kumartrohit@gmail.com>
2020-07-21 19:57:12 +0530

    made changes

--- Website/server.py
+++ Website/server.py
@@ -2,16 +2,13 @@ from flask import Flask, render_template, url_for, request, redirect
 import csv
 app = Flask(__name__)
 
-# @app.route('/')
+
 @app.route('/hello')
 def hello_world():
-    # return 'Hello, Rohit!'
-    # print(url_for('static', filename='icon.png')) # /static/icon.png
+   
     return render_template('indexold.html')
 
-# @app.route('/<username>')
-# def hello_1(username = None):
-#     return render_template('indexold.html', name = username) # to run use {{name}} in html body
+
 
 @app.route('/blog')
 def blog():
@@ -19,24 +16,10 @@ def blog():
 
 
 
-
-# for website
-
 @app.route('/')
 def my_home():
     return render_template('index.html')
 
-# @app.route('/about.html')
-# def about():
-#     return render_template('about.html')
-
-# @app.route('/works.html')
-# def work():
-#     return render_template('works.html')
-
-# @app.route('/contact.html')
-# def contact():
-#     return render_template('contact.html') # Instead of above 3 to avoid DRY. use this below variable rules method
 
 
 @app.route('/<string:page_name>')
